00ca5504e31cd5c05f2a4b962f53f9a4ac8bcb03
ninxfeet <matho.camara@gmail.com>
2019-04-18 08:54:52 +0300

    Add project files.

---
+++ Lab3V20CsharpConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3V20CsharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
