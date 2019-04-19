39d7ede633521a531aa6f27e93c54769fbddd7d0
ninxfeet <matho.camara@gmail.com>
2019-04-19 13:50:22 +0300

    LAB3

---
+++ Lab3V20CsharpConsole/Matrix.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3V20CsharpConsole
+{
+    public class Matrix
+    {
+        // initiate a variable for the matrix size
+        const int sizeMatrix = 3;
+
+        //element in the matrix
+        public int[,] MatrixA;
+        public int[,] MatrixB;
+
+       
+        //print the matrix content
+        public void PrintMatrix(int[,] Amatrix,string name)
+        {
+            //displaying the matrix content
+            Console.WriteLine("Matrix {0}:\n",name);
+
+            for(int i = 0; i < sizeMatrix; i++)
+            {
+                for(int j = 0; j < sizeMatrix; j++)
+                {
+                    Console.Write("{0}\t", Amatrix[i,j]);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+         // caculate the matrix X=2*A(A+B)-3*A
+            public int[,] CalculateMatrixX()
+            {
+            int[,] matrixX = new int[sizeMatrix, sizeMatrix];
+                for (int i = 0; i < sizeMatrix; i++)
+                {
+                    for (int j = 0; j < sizeMatrix; j++)
+                    {
+                    matrixX[i, j] = (2 * MatrixA[i, j]*(MatrixA[i, j]+ MatrixB[i, j]))-(3* MatrixA[i, j]);
+                    }
+                    
+                }            
+            return matrixX;
+            }
+
+        public int[,] ReverserMatrix(int[,] matrixX)
+        {
+            for (int i = 0; i < sizeMatrix; i++)
+            {
+                for (int j = 0; j < sizeMatrix; j++)
+                {
+                    matrixX[i, j] = CalculateMatrixX()[j,i];
+                }
+            }
+            return matrixX;
+        }
+
+        //Return the value IsSymetric
+        public bool IsSymetric(int[,] Matrix,int[,] ReverseMatrix)
+        {
+            var symetric = 0;
+            for (int i = 0; i < sizeMatrix; i++)
+            {
+                for (int j = 0; j < sizeMatrix; j++)
+                {
+                    if (Matrix[i, j] == ReverseMatrix[i,j])
+                        symetric+=1;
+                }
+            }
+            return symetric == sizeMatrix*sizeMatrix;
+        }
+
+        //Constructor
+        public Matrix(int[,] matrixA, int[,] matrixB)
+        {
+            MatrixA = matrixA;
+            MatrixB = matrixB;
+        }
+        //Desctructor
+        ~Matrix()
+        {
+            Console.WriteLine("All Matrixes have been deleted.");
+        }
+
+    }
+}
--- Lab3V20CsharpConsole/Program.cs
+++ Lab3V20CsharpConsole/Program.cs
@@ -10,6 +10,33 @@ namespace Lab3V20CsharpConsole
     {
         static void Main(string[] args)
         {
+            const int size = 3;
+
+            // intializing the matrix
+            Matrix myMatrix = new Matrix(new int[size, size] { { 2, 3, 4 }, { 1, -2, 0 }, { 0, 1, 2 } }, new int[size, size] { { 2, 0, -2 }, { 1, 1, 0 }, { 1, -1, 1 } });
+            //Matrix myMatrix = new Matrix(new int[size, size] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } }, new int[size, size] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } });
+
+            //Printing the matrix content
+            myMatrix.PrintMatrix(myMatrix.MatrixA, "A");
+            Console.WriteLine("\n");
+            myMatrix.PrintMatrix(myMatrix.MatrixB,"B");
+
+            //Printing the MatrixX
+            Console.WriteLine("\n");
+            var matrixX = myMatrix.CalculateMatrixX();
+            myMatrix.PrintMatrix(matrixX, "X=2*A*(A+B)-3*A");
+
+            Console.WriteLine("\n");
+            // Computing and print the reverse matrixe
+            var Reversematrix = myMatrix.ReverserMatrix(myMatrix.CalculateMatrixX());
+            //Printing
+            myMatrix.PrintMatrix(Reversematrix, "Reverse X");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Is Matrix X Symetric ? : {0}",myMatrix.IsSymetric(matrixX, Reversematrix));
+
+            Console.ReadKey();
+
         }
     }
 }
